5d1830e19bbca5b452762f6298b97bbd7036da76
Kayleigh Johnston <kjohnston@oddacity.com>
2015-04-28 08:34:28 -0400

    Style tweaks

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,3 +1,5 @@
+var mastheadH = $('#masthead').outerHeight();
+
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -5,7 +7,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 80
+          scrollTop: target.offset().top - mastheadH
         }, 1000);
         return false;
       }
